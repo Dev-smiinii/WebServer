421bebd6e02e063661f2b9358ab126909c2a9ba6
dev1smiinii <sam.sangmin.jeon@gmail.com>
2023-09-14 17:17:24 +0900

    fix: server.js - DEFAULT 설정 + Middle Ware 탑재

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require(`express`);
 const app = express();
 
+const nunjucks = require(`nunjucks`);
+app.set(`view engine`, `html`);
+nunjucks.configure(`views`, { express: app });
+
+app.use(express.static(`public`));
+app.use(express.urlencoded({ extended: true }));
+
 app.get(`/`, (req, res) => {
   res.send(`ROUTING TO ROOT`);
 });
