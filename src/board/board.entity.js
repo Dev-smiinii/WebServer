b214bd7aee3ffdbde3a7629d73b189ec989002e8
dev1smiinii <sam.sangmin.jeon@gmail.com>
2023-09-15 16:34:41 +0900

    fix: board.entity.js - Entity Settings

--- src/board/board.entity.js
+++ src/board/board.entity.js
@@ -1,5 +1,19 @@
 class Board {
-  constructor() {}
+  id;
+  title;
+  writer;
+  content;
+  created_at;
+  hit;
+
+  constructor(id, title, writer, content) {
+    this.id = id;
+    this.title = title;
+    this.writer = writer;
+    this.content = content;
+    this.created_at = new Date();
+    this.hit = 0;
+  }
 }
 
 module.exports = Board;
