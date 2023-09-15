13017c6346b8d83eef11d261729f8b95425c92f4
dev1smiinii <sam.sangmin.jeon@gmail.com>
2023-09-15 17:27:20 +0900

    (fix: board.repository.js - Repository Settings)

--- src/board/board.repository.js
+++ src/board/board.repository.js
@@ -1,1 +1,31 @@
 const Board = require(`./board.entity`);
+
+const data = [];
+
+exports.findAll = () => {
+  return data; // []
+};
+
+const latestId = () => {
+  return data.length + 1; // []
+};
+
+exports.create = (title, writer, content) => {
+  const id = latestId(); // []
+  const board = new Board(id, title, writer, content);
+  data.push(board); // [{}]
+
+  return board; // [{}]
+};
+
+exports.findOne = (id) => {
+  const [board] = data.filter((row) => {
+    return row.id === id;
+  });
+  return board;
+};
+
+exports.incrementHit = (id) => {
+  const index = data.findIndex((v) => v.id === id);
+  data[index].hit += 1;
+};
