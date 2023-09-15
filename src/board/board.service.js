d0a48719421ef99d958778e82deba5bc3825f8fe
dev1smiinii <sam.sangmin.jeon@gmail.com>
2023-09-15 17:35:11 +0900

    (fix: board.service.js - Service Settings)

--- src/board/board.service.js
+++ src/board/board.service.js
@@ -1,1 +1,21 @@
 const boardRepository = require(`./board.repository`);
+
+exports.getFindAll = () => {
+  const result = boardRepository.findAll(); // []
+  return result; // []
+};
+
+exports.create = (data) => {
+  const { title, writer, content } = data; // {}
+  const result = boardRepository.create(title, writer, content); // [{}]
+  return result; // [{}]
+};
+
+exports.getFindOne = (board_id) => {
+  const id = parseInt(board_id); // num
+  const result = boardRepository.findOne(id); // num
+
+  boardRepository.incrementHit(id); // num
+
+  return result; // num
+};
